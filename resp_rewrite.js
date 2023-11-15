0eb8193c0abe4282b781ddd204410091b7f33591
txw-yates <txwyates@gmail.com>
2023-11-15 18:28:37 +0800

    Update resp_rewrite.js

--- resp_rewrite.js
+++ resp_rewrite.js
@@ -1,26 +1,26 @@
-function modifyMain(url, data) {
+function modifyMain(url, str) {
   if(url.indexOf('/product/list') > -1) { // 相关过滤，红宝书广告
     try {
       let data = JSON.parse(data);
-      data["data"] = "[]"
+      data["data"] = []
       return JSON.stringify(data);
     } catch (error) {
       console.log(`红宝书广告, 出现异常: ` + error);
     }
-    return data
+    return str
   } else if(url.indexOf('/w1/api/index.php') > -1) { // 开盘啦，首页广告位置
     try {
       let data = JSON.parse(data);
-      data["List"] = "[]";
-      data["IndexAd"] = "[]";
-      data["Advert"] = "[]";
+      data["List"] = [];
+      data["IndexAd"] = [];
+      data["Advert"] = [];
       return JSON.stringify(data);
     } catch (error) {
       console.log(`开盘啦，首页广告位置, 出现异常: ` + error);
     }
-    return data
+    return str
   }
-	return data;
+	return str;
 }
 
 var body = $response.body;
