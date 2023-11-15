0436ad27b677cb596148f7aa7c69e3554607e5e5
txw-yates <txwyates@gmail.com>
2023-11-15 17:57:05 +0800

    Update resp_rewrite.js

--- resp_rewrite.js
+++ resp_rewrite.js
@@ -1,15 +1,25 @@
 function modifyMain(url, data) {
-	if(url.indexOf('/product/list') > -1) { // 相关过滤，红宝书广告
-    data = JSON.parse(data);
-		data["data"] = "[]"
-    return JSON.stringify(data);
-	} else if(url.indexOf('/w1/api/index.php') > -1) { // 开盘啦，首页广告位置
-		data = JSON.parse(data);
-		data["List"] = "[]";
-    data["IndexAd"] = "[]";
-    data["Advert"] = "[]";
-		return JSON.stringify(data);
-	}
+  if(url.indexOf('/product/list') > -1) { // 相关过滤，红宝书广告
+    try {
+      let data = JSON.parse(data);
+      data["data"] = "[]"
+      return JSON.stringify(data);
+    } cache (error) {
+      console.log(`红宝书广告, 出现异常: ` + error);
+    }
+    return data
+  } else if(url.indexOf('/w1/api/index.php') > -1) { // 开盘啦，首页广告位置
+    try {
+      let data = JSON.parse(data);
+      data["List"] = "[]";
+      data["IndexAd"] = "[]";
+      data["Advert"] = "[]";
+      return JSON.stringify(data);
+    } cache (error) {
+      console.log(`开盘啦，首页广告位置, 出现异常: ` + error);
+    }
+    return data
+  }
 	return data;
 }
 
